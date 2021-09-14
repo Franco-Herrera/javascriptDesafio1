a5ad060c1a8b9096c33e6b7127ec34925e0be555
Franco-Herrera <franco2001herrera@gmail.com>
2021-09-14 01:42:24 -0300

    ya hecho el script

---
+++ app.js
@@ -0,0 +1,9 @@
+
+const blanco = " ";
+
+let Nombre = prompt("Dinos Tu Nombre");
+let Apellido = prompt("Dinos Tu Apellido");
+
+alert( "Bienvenido" + blanco + Nombre + blanco + Apellido + "!");
+
+
