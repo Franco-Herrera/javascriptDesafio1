94c291a664875ec7058ff4ed5edbe1101245b9d2
Franco-Herrera <franco2001herrera@gmail.com>
2021-09-22 23:23:24 -0300

    tarea condicionales

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 
-const blanco = " ";
+let numeroDeAbordaje = prompt ("Tu Numero De Abordaje");
 
-let Nombre = prompt("Dinos Tu Nombre");
-let Apellido = prompt("Dinos Tu Apellido");
+alert( " Bienvenido, Tu numero de asiento es el " + numeroDeAbordaje + "!");
 
-alert( "Bienvenido" + blanco + Nombre + blanco + Apellido + "!");
-
-
+if (numeroDeAbordaje <= 10) {
+    alert ( "dirigete al asiento por el pasillo a");
+}
+else if (numeroDeAbordaje <= 19) {
+    alert ( "dirigete al asiento por el pasillo b");
+}
+else if (numeroDeAbordaje <= 30) {
+    alert ( "dirigete al asiento por el pasillo c");
+}
+else if (numeroDeAbordaje > 31) {
+    alert(" has ingresado mal el numero de abordaje, intentelo nuevamente")
+}
