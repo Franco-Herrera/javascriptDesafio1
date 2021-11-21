bbb51ec199e4120798a0f510f9a2c34bcac88960
Franco-Herrera <franco2001herrera@gmail.com>
2021-11-21 18:34:01 -0300

    tarea desafio 1

--- app.js
+++ app.js
@@ -1,17 +1,2 @@
-
-let numeroDeAbordaje = prompt ("Tu Numero De Abordaje");
-
-alert( " Bienvenido, Tu numero de asiento es el " + numeroDeAbordaje + "!");
-
-if (numeroDeAbordaje <= 10) {
-    alert ( "dirigete al asiento por el pasillo a");
-}
-else if (numeroDeAbordaje <= 19) {
-    alert ( "dirigete al asiento por el pasillo b");
-}
-else if (numeroDeAbordaje <= 30) {
-    alert ( "dirigete al asiento por el pasillo c");
-}
-else if (numeroDeAbordaje > 31) {
-    alert(" has ingresado mal el numero de abordaje, intentelo nuevamente")
-}
+let NombreDeUsuario = prompt("Dinos Tu Nombre");
+alert ("Bienvenido " + NombreDeUsuario + "!");
