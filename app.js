f6f3828198958d64a994d9ec5c3841c7c3b46cd6
Franco-Herrera <franco2001herrera@gmail.com>
2021-11-24 18:47:26 -0300

    tarea javascript 2

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
 let NombreDeUsuario = prompt("Dinos Tu Nombre");
-alert ("Bienvenido " + NombreDeUsuario + "!");
+let NumeroAsiento = prompt("Dinos Tu asiento");
+
+if (NumeroAsiento < 20){
+    alert ( NombreDeUsuario + " dirigase por el pasillo a ")
+}
+else if (NumeroAsiento < 40){
+    alert ( NombreDeUsuario + " dirigase por el pasillo b ")
+}
+else if (NumeroAsiento > 41){
+    alert (" Reingrese el numero de asiento, este no existe ")
+}
