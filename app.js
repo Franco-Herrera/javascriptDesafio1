3e43664c521443c718b86a49a3720c3928d45062
Franco-Herrera <franco2001herrera@gmail.com>
2021-11-29 21:18:47 -0300

    tarea ciclos

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/*
 let NombreDeUsuario = prompt("Dinos Tu Nombre");
 let NumeroAsiento = prompt("Dinos Tu asiento");
 
@@ -10,3 +11,31 @@ else if (NumeroAsiento < 40){
 else if (NumeroAsiento > 41){
     alert (" Reingrese el numero de asiento, este no existe ")
 }
+
+
+for(let i = 1; i<=10; i++) {
+    console.log(i)
+}
+
+*/
+alert (" coloque los precios de sus productos y los calcularemos en el carrito y su envio")
+
+let repetir = true;
+let acumulador = 0;
+
+while(repetir) {
+    let totalCarrito = parseInt(prompt("ingresa precio del producto al carrito"))
+    if(totalCarrito >= 300 && totalCarrito <=1500) {
+        acumulador += totalCarrito
+    } else {
+        alert ("precio incorrecto")
+    }
+
+    let continua = prompt("desea agregar otro producto al carrito?").toLowerCase()
+
+    if(continua == "no") {
+        repetir = false;
+    }
+}
+
+alert (`el total de la compra es ${acumulador} y el envio ${acumulador * 0.15}`)
