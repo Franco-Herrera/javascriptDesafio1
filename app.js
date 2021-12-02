b430a82013ea8d5c669acce35efffed03ddbcec1
Franco-Herrera <franco2001herrera@gmail.com>
2021-12-01 21:08:59 -0300

    tarea funciones javascript

--- app.js
+++ app.js
@@ -17,7 +17,6 @@ for(let i = 1; i<=10; i++) {
     console.log(i)
 }
 
-*/
 alert (" coloque los precios de sus productos y los calcularemos en el carrito y su envio")
 
 let repetir = true;
@@ -39,3 +38,50 @@ while(repetir) {
 }
 
 alert (`el total de la compra es ${acumulador} y el envio ${acumulador * 0.15}`)
+
+*/
+const precioBaseCorreo = 200
+const precioHasta5 = 300
+const precioHasta15 = 700
+const precioHasta30 = 1200
+const IVA = 1.21
+const envioNacional = 1.25
+const envioInternacional = 1.50
+
+
+function envioCorreo (NombreDeUsuario){
+
+    let cuentaPeso = 0 ;
+    //let cuentaEnvioNacional = 0 ;
+    //let cuentaEnvioInternacional = 0 ;
+    
+    let resultado = 0 ;
+
+    let pesoPaquete = parseInt(prompt("de cuanto kilos es el peso del paquete a enviar?"))
+
+    if(pesoPaquete >=1 && pesoPaquete <= 5){
+        cuentaPeso = precioBaseCorreo + precioHasta5
+    }
+    else if(pesoPaquete >=6 && pesoPaquete <= 15){
+        cuentaPeso = precioBaseCorreo + precioHasta15
+
+    }
+    else if(pesoPaquete >=16 && pesoPaquete <= 30){
+        cuentaPeso = precioBaseCorreo + precioHasta30
+    
+    }   
+    alert( " el recargo por el peso es de $" + cuentaPeso)
+    
+    let claseEnvio = prompt("Su envio es de clase nacional o internacional?").toLowerCase() 
+    if(claseEnvio == "nacional"){
+        resultado = (cuentaPeso * envioNacional * IVA)
+    }
+    else if(claseEnvio == "internacional"){
+        resultado = (cuentaPeso * envioInternacional * IVA)
+    }
+    
+    alert ( NombreDeUsuario + " , el precio de tu envio es de $ " + resultado)
+}
+let NombreDeUsuario = prompt ("dinos tu nombre")
+envioCorreo (NombreDeUsuario)
+
